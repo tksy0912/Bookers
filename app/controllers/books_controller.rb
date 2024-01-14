1332110562f5d3cc6761477e43934771c7964701
EC2 Default User <ec2-user@ip-172-31-15-227.ap-northeast-1.compute.internal>
2024-01-14 03:36:03 +0000

    [Update] レイアウト

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -22,7 +22,7 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
   end
 
-def update
+  def update
     book = Book.find(params[:id])
     book.update(book_params)
     redirect_to book_path(book.id)  
